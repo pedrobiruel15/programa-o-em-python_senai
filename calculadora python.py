8b978d8ed8b5f617efd0c94de32fb2c99e72a976
pedrobiruel15 <biruelpedro@gmail.com>
2026-06-12 17:52:19 -0300

    Add files via upload

---
+++ calculadora python.py
@@ -0,0 +1,8 @@
+numero1 = float(input('numero 1: '))
+numero2 = float(input('numero 2: '))
+
+print (numero1 + numero2)
+print (numero1 - numero2)
+print (numero1 * numero2)
+print (numero1 / numero2)
+
