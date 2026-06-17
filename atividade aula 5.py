7ac6a25a0126c6c0b9ffcbefd0c6e0f1e9c8b7e8
pedrobiruel15 <biruelpedro@gmail.com>
2026-06-17 14:41:19 -0300

    Add files via upload

---
+++ atividade aula 5.py
@@ -0,0 +1,48 @@
+
+# 1
+
+print('sejam bem vindos!')
+
+# 2
+
+valor = True
+print(valor)
+
+# 3
+
+resultado = (5.0 * 5.69)
+print(resultado)
+
+# 4
+
+resultado = (60 / 2)
+print(resultado)
+
+# 5
+
+resultado = (100 - 99)
+print(resultado)
+
+# 6
+
+resultado = (35 // 5)
+print(resultado)
+
+# 7
+
+resultado = (43.2 * 7.1 * 89.6 * 1.9)
+print(resultado)
+
+# 8
+
+numero = 7
+print(numero * 2)
+
+# 9
+
+nome = (input('nome: lionel'))
+email = (input('email: lionelbom1234'))
+idade = (input('idade: 57'))
+cidade = (input('cidade: guarulhos'))
+graduação = (input('graduação: medicina'))
+estado_civil  = (input('estado_civil : casado'))
