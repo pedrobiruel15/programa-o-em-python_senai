b16a6084a937f68aa88e5c91a7e014130259b488
pedrobiruel15 <biruelpedro@gmail.com>
2026-06-18 16:29:27 -0300

    Add files via upload

---
+++ atividade aula 7.py
@@ -0,0 +1,27 @@
+# 0
+
+for a in range(2,21, 2):
+     print(a)
+
+# 1
+
+numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+print(numeros)
+
+print(numeros[2])
+
+numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+print(numeros)
+
+numeros = [1, 2, 3, 4, 6, 7, 8, 9, 9, 10]
+
+print(numeros)
+
+carros = ['bmw, bugati, tuc tuc']
+resultado = carros = numeros
+print(resultado)
+
+
+
