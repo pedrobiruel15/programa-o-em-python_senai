6332050a1edeffa7dc5eb47a1fa463ecd4934c26
pedrobiruel15 <biruelpedro@gmail.com>
2026-06-19 17:55:38 -0300

    Add files via upload

---
+++ atividade 8.py
@@ -0,0 +1,28 @@
+# 1
+
+numero = float(input("1)digite um numero"))
+
+if numero > 1:
+    print('positivo')
+if numero < 0:
+    print ('negativo')
+if numero == 0:
+    print('zero')
+
+# 2
+
+idade = float(input('2)digite sua idade'))
+
+if idade >= 18:
+    print('pode votar')
+if idade < 18:
+    print('não pode votar')
+
+# 3
+
+numero = int(input('3) coloque um numero'))
+
+if numero % 2 == 0:
+    print('numero par')
+else:
+    print('numero impar')
